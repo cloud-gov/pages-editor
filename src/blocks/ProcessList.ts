c613d0e3c9141e519457a01e95e4e664c5d70d25
Kevin Masters <kevin.masters@gsa.gov>
2025-12-09 16:11:32 -0500

    fix: allow heading level selection in process list feature
    fix: improve heading level and text ui
    
    fix: set heading level to one field, update labels and description

--- src/blocks/ProcessList.ts
+++ src/blocks/ProcessList.ts
@@ -6,11 +6,36 @@ export const ProcessListBlock: Block = {
   labels: { singular: 'Process List', plural: 'Process Lists' },
   fields: [
     {
+      name: 'headingLevel',
+      type: 'select',
+      defaultValue: 'h4',
+      options: [
+        { label: 'Heading 2', value: 'h2' },
+        { label: 'Heading 3', value: 'h3' },
+        { label: 'Heading 4', value: 'h4' },
+        { label: 'Heading 5', value: 'h5' },
+        { label: 'Heading 6', value: 'h6' },
+      ],
+      admin: {
+        description:
+          'Pick the heading level that\'s one step down from your last heading (H4 is selected by default).',
+      },
+      required: true,
+    },
+    {
       name: 'items',
       type: 'array',
       admin: { initCollapsed: false },
       fields: [
-        { name: 'heading', type: 'text', required: true },
+       { 
+          name: 'heading',
+          type: 'text',
+          required: true,
+          admin: {
+            width: '75%',
+            description: 'Heading text',
+          }
+        },
         {
           name: 'body',
           type: 'richText',
@@ -19,7 +44,7 @@ export const ProcessListBlock: Block = {
               ...defaultFeatures,
             ]
           })
-        }
+        },
       ]
     }
   ]
