8acb63e8e053d31e9d2470a86a7aebd321a72bbc
Vini Moret <vini.moret@gsa.gov>
2026-05-27 11:07:19 -0400

    fix: add endpoint

--- src/endpoints/index.ts
+++ src/endpoints/index.ts
@@ -1,9 +1,9 @@
 import siteSelect from './siteSelect';
 import removeUser from './removeUser';
 import siteWebhook from './siteWebhook';
-
+import publishedBuildStatusWebhook from './publishedBuildStatusWebhook'
 const endpoints = [
-    siteSelect, removeUser, siteWebhook,
+    siteSelect, removeUser, siteWebhook, publishedBuildStatusWebhook,
 ]
 
 export default endpoints
