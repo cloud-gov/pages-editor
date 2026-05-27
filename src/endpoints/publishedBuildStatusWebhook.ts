606d03a016f2c25f9adc8d55d27b15ab887858b1
Vini Moret <vini.moret@gsa.gov>
2026-05-27 10:47:01 -0400

    feat: add published build status webhook

---
+++ src/endpoints/publishedBuildStatusWebhook.ts
@@ -0,0 +1,75 @@
+import { decrypt } from '@/utilities/encryptor'
+import type { Config, PayloadRequest } from 'payload'
+
+const post: Required<Config>["endpoints"][number]["method"] = "post"
+
+const endpoint = {
+    path: '/webhook/buildStatus/:buildId',
+    method: post,
+    handler: async (req: PayloadRequest & { json: CallableFunction, routeParams: { id?: string } }) => {
+      const { buildId } = req.routeParams
+      if (!buildId) return Response.json({ message: 'error' })
+      const {
+        siteId: encryptedPagesSiteId,
+        state: encryptedState,
+        startedAt: encryptypedStartedAt,
+        completedAt: encryptypedCompletedAt,
+        error: encryptypedError
+      } = await req.json()
+      try {
+        const pagesSiteId = Number(decrypt(encryptedPagesSiteId, process.env.PAGES_ENCRYPTION_KEY))
+        const state = decrypt(encryptedState, process.env.PAGES_ENCRYPTION_KEY)
+        const startedAt = decrypt(encryptypedStartedAt, process.env.PAGES_ENCRYPTION_KEY)
+        const completedAt = decrypt(encryptypedCompletedAt, process.env.PAGES_ENCRYPTION_KEY)
+        const error = decrypt(encryptypedError, process.env.PAGES_ENCRYPTION_KEY)
+
+        const site = await req.payload.findByID({
+          collection: 'sites',
+          id: pagesSiteId
+        })
+
+        const publishedBuildStatusExists = await req.payload.find({
+          collection:'published-build-status',
+          where:{
+            pagesBuildId:{
+              equals: buildId
+            }
+          },
+          req
+        })
+
+        if(!publishedBuildStatusExists || publishedBuildStatusExists.totalDocs === 0){
+          const publishedBuildStatus = await req.payload.create({
+            collection: 'published-build-status',
+            data: {
+              site,
+              startedAt,
+              completedAt,
+              state,
+              pagesBuildId: Number(buildId),
+              error
+            }
+          })
+          return Response.json(publishedBuildStatus)
+      }else{
+        const updatedUser = await payload.update({
+          collection: 'published-build-status',
+          id: publishedBuildStatusExists[0].id,
+          data: {
+            completedAt,
+            error,
+            state,
+          },
+          req
+        })
+        return Response.json(updatedUser)
+      }
+        
+      } catch (err) {
+        console.error(err)
+        return Response.json({ message: 'error updating site' })
+      }
+    }
+}
+
+export default endpoint
